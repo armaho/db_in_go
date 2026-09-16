50c3a39ec741e2a383b6ec9b75668503f1df2d3b
Arman Hosseini <armanhosseini878787@gmail.com>
2026-09-16 16:23:34 +0330

    rewrite parser

--- sql_parser.go
+++ sql_parser.go
@@ -12,103 +12,77 @@ type Parser struct {
 }
 
 func NewParser(s string) Parser {
-	return Parser{buf: strings.TrimSpace(s), pos: 0}
+	return Parser{buf: s, pos: 0}
 }
 
-func (p *Parser) advance() (ch byte, ok bool) {
-	if p.pos >= len(p.buf) {
-		return
+func isSpace(ch byte) bool {
+	switch ch {
+	case '\t', '\n', '\v', '\f', '\r', ' ':
+		return true
 	}
-
-	p.pos += 1
-	if p.pos == len(p.buf) {
-		return
-	}
-
-	ok = true
-	ch = p.current()
-	return
+	return false
+}
+func isAlpha(ch byte) bool {
+	return 'a' <= (ch|32) && (ch|32) <= 'z'
+}
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+func isNameStart(ch byte) bool {
+	return isAlpha(ch) || ch == '_'
+}
+func isNameContinue(ch byte) bool {
+	return isAlpha(ch) || isDigit(ch) || ch == '_'
+}
+func isSeparator(ch byte) bool {
+	return ch < 128 && !isNameContinue(ch)
 }
 
-func (p *Parser) isEnd() bool {
-	return p.pos >= len(p.buf)
-}
-
-func (p *Parser) current() byte {
-	return p.buf[p.pos]
-}
-
-func (p *Parser) skipSpace() {
-	for !p.isEnd() && isSpace(p.current()) {
-		p.advance()
+func (p *Parser) skipSpaces() {
+	for p.pos < len(p.buf) && isSpace(p.buf[p.pos]) {
+		p.pos += 1
 	}
-}
-
-func (p *Parser) tryName() (string, bool) {
-	if p.isEnd() || !isNameStart(p.current()) {
-		return "", false
-	}
-
-	start := p.pos
-	end := p.pos + 1
-	c, ok := p.advance()
-	for ok && isNameContinue(c) {
-		end += 1
-		c, ok = p.advance()
-	}
-
-	p.skipSpace()
-	return p.buf[start:end], true
 }
 
 func (p *Parser) tryKeyword(kw string) bool {
-	if len(kw)+p.pos > len(p.buf) ||
-		(len(kw)+p.pos != len(p.buf) && !isSeparator(p.buf[p.pos+len(kw)])) {
+	p.skipSpaces()
+	if !(p.pos+len(kw) <= len(p.buf) && strings.EqualFold(p.buf[p.pos:p.pos+len(kw)], kw)) {
 		return false
 	}
-
-	kw = strings.ToLower(kw)
-	sub_str := strings.ToLower(p.buf[p.pos : p.pos+len(kw)])
-	if kw != sub_str {
+	if p.pos+len(kw) < len(p.buf) && !isSeparator(p.buf[p.pos+len(kw)]) {
 		return false
 	}
-
 	p.pos += len(kw)
-	p.skipSpace()
 	return true
 }
 
-func (p *Parser) parseInt(out *Cell) error {
-	start := p.pos
-	end := p.pos + 1
-	ch, ok := p.current(), true
-	if ch == '-' || ch == '+' {
-		ch, ok = p.advance()
-		end += 1
+func (p *Parser) tryName() (string, bool) {
+	p.skipSpaces()
+	start, cur := p.pos, p.pos
+	if !(cur < len(p.buf) && isNameStart(p.buf[cur])) {
+		return "", false
 	}
-	if !isDigit(ch) {
-		return errors.New("expected digit after +/-")
+	cur++
+	for cur < len(p.buf) && isNameContinue(p.buf[cur]) {
+		cur++
 	}
-	for ok && isDigit(ch) {
-		ch, ok = p.advance()
-		if ok {
-			end += 1
-		}
-	}
+	p.pos = cur
+	return p.buf[start:cur], true
+}
 
-	if !p.isEnd() && !isSeparator(p.current()) {
-		return errors.New("expected seperator after num")
+func (p *Parser) parseValue(out *Cell) error {
+	p.skipSpaces()
+	if p.pos >= len(p.buf) {
+		return errors.New("expect value")
 	}
-
-	p.skipSpace()
-	num, err := strconv.Atoi(p.buf[start:end])
-	if err != nil {
-		panic("cannot parse num")
+	ch := p.buf[p.pos]
+	if ch == '"' || ch == '\'' {
+		return p.parseString(out)
+	} else if isDigit(ch) || ch == '-' || ch == '+' {
+		return p.parseInt(out)
+	} else {
+		return errors.New("expect value")
 	}
-	out.Type = TypeI64
-	out.I64 = int64(num)
-
-	return nil
 }
 
 func (p *Parser) parseString(out *Cell) error {
@@ -136,41 +110,26 @@ func (p *Parser) parseString(out *Cell) error {
 	return errors.New("string is not terminated")
 }
 
-func (p *Parser) parseValue(out *Cell) error {
-	if p.isEnd() {
-		return errors.New("expect value")
+func (p *Parser) parseInt(out *Cell) (err error) {
+	start, cur := p.pos, p.pos
+	if p.buf[cur] == '-' || p.buf[cur] == '+' {
+		cur++
 	}
-	ch := p.current()
-	if ch == '"' || ch == '\'' {
-		return p.parseString(out)
-	} else if isDigit(ch) || ch == '-' || ch == '+' {
-		return p.parseInt(out)
-	} else {
-		return errors.New("expect value")
+	for cur < len(p.buf) && isDigit(p.buf[cur]) {
+		cur++
 	}
+
+	if out.I64, err = strconv.ParseInt(p.buf[start:cur], 10, 64); err != nil {
+		return err
+	}
+	out.Type = TypeI64
+	p.pos = cur
+	return nil
 }
 
-func isSeparator(ch byte) bool {
-	return ch < 128 && !isNameContinue(ch)
+func (p *Parser) isEnd() bool {
+	p.skipSpaces()
+	return p.pos >= len(p.buf)
 }
 
-func isSpace(ch byte) bool {
-	switch ch {
-	case '\t', '\n', '\v', '\f', '\r', ' ':
-		return true
-	}
-	return false
-}
-
-func isAlpha(ch byte) bool {
-	return 'a' <= (ch|32) && (ch|32) <= 'z'
-}
-func isDigit(ch byte) bool {
-	return '0' <= ch && ch <= '9'
-}
-func isNameStart(ch byte) bool {
-	return isAlpha(ch) || ch == '_'
-}
-func isNameContinue(ch byte) bool {
-	return isAlpha(ch) || isDigit(ch) || ch == '_'
-}
+// QzBQWVJJOUhU https://trialofcode.org/
